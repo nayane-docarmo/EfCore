364a54186efa159256a135a8e864247bdc2cfc56
nayane-docarmo <nayane.docarmo02@gmail.com>
2020-12-16 14:29:10 -0300

    Projeto concluido

---
+++ Senai.EfCore/Contexts/PedidoContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Senai.EfCore.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.EfCore.Contexts
+{
+    public class PedidoContext : DbContext
+    {
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<PedidoItem> PedidosItens { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(@"Data Source=.\SqlExpress2; Initial Catalog=Db_Senai_Pedidos_Dev; user id=sa; password=sa132");
+            base.OnConfiguring(optionsBuilder); 
+        }
+    }
+}
---
+++ Senai.EfCore/Domains/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.EfCore.Domains
+{
+    public class Produto
+    {
+        /// <summary>
+        /// Define a classe produto
+        /// </summary>
+
+        [Key]
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+
+        public Produto()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
